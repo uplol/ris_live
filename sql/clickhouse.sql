91517556cbfbb7bc30908d575241d5b1f1b9ac29
KryptosKid <KryptosKid@users.noreply.github.com>
2022-10-18 06:25:39 +0000

    ref: fix prefixes and add `originating_asn` column

--- sql/clickhouse.sql
+++ sql/clickhouse.sql
@@ -13,12 +13,13 @@ create table if not exists bgp.ris_live
         next_hop String,
         prefixes Array(String)
     ),
-    as_set Array(Int32),
+    as_set Array(Int32), --
     community Array(Array(Nullable(UInt32))),
     host String,
     id String,
     med Nullable(UInt32),
     origin Nullable(String),
+    originating_asn UInt32, --
     path Array(Nullable(UInt32)),
     peer String,
     peer_asn String,
@@ -32,8 +33,9 @@ ORDER BY (timestamp, id);
 create materialized view if not exists bgp.ris_live_mv to bgp.ris_live
 as select
     JSONExtractArrayRaw(data, 'announcements') as announcements_,
+    arrayMap(c -> (JSONExtractArrayRaw(c, 'prefixes')), announcements_) prefixes_,
     arrayMap(c -> (JSONExtractString(c, 'next_hop')), announcements_) as `announcements.next_hop`,
-    arrayMap(c -> (JSONExtractArrayRaw(c, 'prefixes')), announcements_) as `announcements.prefixes`,
+    arrayMap(a -> (arrayMap(b ->(trim(BOTH '"' FROM b)), a)), prefixes_) as `announcements.prefixes`,
     JSONExtractString(data, 'aggregator') as aggregator,
     JSONExtractArrayRaw(data, 'as_set')::Array(UInt32) as as_set,
     arrayMap(c -> (c::Array(UInt32)), JSONExtractArrayRaw(data, 'community')) as community,
@@ -42,6 +44,7 @@ as select
     JSONExtractUInt(data, 'med') as med,
     JSONExtractString(data, 'origin') as origin,
     JSONExtractArrayRaw(data, 'path')::Array(UInt32) as path,
+    path[-1] as originating_asn,
     JSONExtractString(data, 'peer') as peer,
     JSONExtractString(data, 'peer_asn') as peer_asn,
     JSONExtractString(data, 'timestamp') as timestamp,
